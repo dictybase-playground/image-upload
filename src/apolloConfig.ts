01bb947629da888c7f7f0e1382bc5a0bad602d1e
Kevin Tun <kevin.tun@northwestern.edu>
2024-04-12 12:08:33 -0500

    add /graphql

--- src/apolloConfig.ts
+++ src/apolloConfig.ts
@@ -2,7 +2,7 @@ import { InMemoryCache } from "@apollo/client";
 import createUploadLink from "apollo-upload-client/createUploadLink.mjs";
 
 const uploadLink = createUploadLink({
-  uri: `${import.meta.env.VITE_GRAPHQL_SERVER}/graphql`,
+  uri: `${import.meta.env.VITE_GRAPHQL_SERVER}/graphqlg`,
 });
 const config = {
   cache: new InMemoryCache(),
